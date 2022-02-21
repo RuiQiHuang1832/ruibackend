bf72295cad077d186767d65723941974195a250f
Ben <rhuang10@mail.sfsu.edu>
2022-02-21 13:40:41 -0800

    test

--- src/main/java/com/ruibackend/ruiwebappbackend/security/WebSecurityConfiguration.java
+++ src/main/java/com/ruibackend/ruiwebappbackend/security/WebSecurityConfiguration.java
@@ -49,12 +49,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/**").permitAll();
-//    }
 
     @Bean
     AuthenticationProvider authenticationProvider() {
